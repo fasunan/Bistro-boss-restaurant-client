18691059221a44bfc36ac2a32da31cd8caf427cd
fasunan <fasunan@gmail.com>
2023-11-22 13:32:54 +0600

    module-65 done

--- src/Pages/Home/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/Home/PopularMenu/PopularMenu.jsx
@@ -1,17 +1,11 @@
-import { useEffect, useState } from "react";
 import SecTionTitle from "../../../../Components/SectionTitle/SecTionTitle";
 import MenuItem from "../../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      });
-  }, []);
+  const [menu]= useMenu();
+  const popularItems = menu.filter((item) => item.category === "popular");
+  
   return (
     <section className="mb-12">
       <SecTionTitle
@@ -19,7 +13,7 @@ const PopularMenu = () => {
         subHeading={"Check it out"}
       ></SecTionTitle>
       <div className="grid md:grid-cols-2 gap-10">
-        {menu.map((item) => (
+        {popularItems.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Layout/Root";
 import Home from "./Pages/Home/Home/Home";
+import { HelmetProvider } from "react-helmet-async";
+import Menu from "./Menu/Menu/Menu";
+import Order from "./Pages/Order/Order/Order";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,14 +16,25 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
+      {
+        path: "/menu",
+        element: <Menu></Menu>,
+      },
+      {
+        path: "/order",
+        element: <Order></Order>,
+      },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="max-w-screen-lg mx-auto">
-      <RouterProvider router={router} />
-    </div>
+
+    <HelmetProvider>
+      <div className="max-w-screen-lg mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
   </React.StrictMode>
 );
